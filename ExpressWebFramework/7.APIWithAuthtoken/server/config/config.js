514f3e0838433982183c850271f9e242ef22e3f5
Oriol Amat <oriol.amat@gmail.com>
2018-12-13 00:07:56 +0100

    docker

--- ExpressWebFramework/7.APIWithAuthtoken/server/config/config.js
+++ ExpressWebFramework/7.APIWithAuthtoken/server/config/config.js
@@ -1,9 +1,10 @@
-var env = process.env.NODE_ENV || 'development';
 
-if (env === 'development') {
+console.log(process.env.MONGO_URL);
+if (process.env.MONGO_URL != null) {  //For Docker
+  process.env.MONGODB_URI = process.env.MONGO_URL;
   process.env.PORT = 3000;
+} else if (env === 'test') {   //For Dev test
   process.env.MONGODB_URI = 'mongodb://localhost:27017/test';
-} else if (env === 'test') {
   process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/test';
 }
+
