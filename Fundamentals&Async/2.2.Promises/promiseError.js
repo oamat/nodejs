692dc1ede7997ba1720e54a1cef4fa188f7f0007
oamatf <oriol.amat@gmail.com>
2019-08-09 17:01:55 +0200

    errorcallback

--- Fundamentals&Async/2.2.Promises/promiseError.js
+++ Fundamentals&Async/2.2.Promises/promiseError.js
@@ -1,6 +1,7 @@
-async function promiseErr() {
+async function promiseErr(entry) {
     return new Promise((resolve, reject) => {
-        throw new Error(' promiseErr  Error throw in normal Promise');
+        if (entry) reject(new Error(' promiseErr  REJECT in normal Promise way  '));
+        else throw new Error(' promiseErr  ERROR throw in normal Promise way ');
     });
 }
 
@@ -9,10 +10,8 @@ async function promiseCallback(entry) {
         setTimeout(() => {  //añadimos asincronia Callback
             //logic code, BBDD acces for example
             try {
-                if (entry) resolve(' promiseCallback resolved ');
-                else {
-                    throw new Error(' promiseCallback Error throw in callback');
-                }
+                if (entry) reject(new Error(' promiseCallback REJECT in callback '));
+                else throw new Error(' promiseCallback ERROR throw in callback');
             } catch (error) {
                 reject(error);
             }
@@ -23,32 +22,56 @@ async function promiseCallback(entry) {
 
 const start = async function () {
     try {
-        console.log("call await promiseErr with Error");
-        await promiseErr()
-            .then((result) => {
-                console.log('Success: ', result);
-            })
+        console.log("call await promiseErr REJECT");
+        await promiseErr(true)
+            .then((result) => { console.log('Success: ', result); })
             .catch((error) => {
-                console.log('catch promiseErr Error INSIDE promise ');
+                console.log('catch promiseErr REJECT INSIDE promise ');
                 throw error;
             });
     } catch (error) {
-        console.log('catch promiseErr Error OUTSIDE promise ');
+        console.log('catch promiseErr REJECT OUTSIDE promise ');
         console.log("error.message : " + error.message);
     }
 
     try {
-
-        console.log("call await promiseCallback with error");
-        await promise(false)
-            .then((result) => {
-                console.log('Success: ', result);
-            }, error => {
-                console.log('catch promiseCallback Error INSIDE promise');
+        console.log("call await promiseErr ERROR throw");
+        await promiseErr(false)
+            .then((result) => { console.log('Success: ', result); })
+            .catch((error) => {
+                console.log('catch promiseErr ERROR throw INSIDE promise ');
                 throw error;
             });
     } catch (error) {
-        console.log('catch promiseCallback Error OUTSIDE  promise ');
+        console.log('catch promiseErr ERROR throw OUTSIDE promise ');
+        console.log("error.message : " + error.message);
+    }
+
+    try {
+        console.log("call await promiseCallback REJECT");
+        await promiseCallback(true)
+            .then((result) => {
+                console.log('Success: ', result);
+            }, error => {
+                console.log('catch promiseCallback REJECT INSIDE promise');
+                throw error;
+            });
+    } catch (error) {
+        console.log('catch promiseCallback REJECT OUTSIDE  promise ');
+        console.log("error.message : " + error.message);
+    }
+
+    try {
+        console.log("call await promiseCallback ERROR Throw");
+        await promiseCallback(false)
+            .then((result) => {
+                console.log('Success: ', result);
+            }, error => {
+                console.log('catch promiseCallback ERROR throw INSIDE promise');
+                throw error;
+            });
+    } catch (error) {
+        console.log('catch promiseCallback ERROR throw OUTSIDE  promise ');
         console.log("error.message : " + error.message);
     }
 
