5786e14a61f8cd2e7c32411356425cb994f4443f
Oriol Amat <oriol.amat@gmail.com>
2019-09-27 00:08:35 +0200

    callbacksAllErr

--- Fundamentals&Async/2.2.Promises/promiseErrCallback.js
+++ Fundamentals&Async/2.2.Promises/promiseErrCallback.js
@@ -1,4 +1,4 @@
-var errPromise = async () => {
+var errPromise = async () => {    
     return new Promise((resolve, reject) => {
         reject(new Error(" reject Promise"));
     });
@@ -20,8 +20,7 @@ var errAsyncFunctionCallback = async () => {
     setTimeout(() => {
         console.log("we cannot catch the err in AsyncFunctionCallback without Promise");
         //throw new Error("ww"); Uncaugcht exception
-        return new Error(" throwErr AsyncFunctionCallback"); 
-               
+        return new Error(" throwErr AsyncFunctionCallback");                
     }, 10);
 
 }
@@ -31,36 +30,40 @@ var errAsyncFunctionCallback = async () => {
 const init = async () => {
     try {
         errPromise().catch((error) => {
-            console.log(error.message);
+            console.log("catch1 " + error.message);
+            //throw error;
         });
     } catch (error) {
-        console.log(error.message);
+        console.log("catch2 " + error.message);
     }
 
     try {
 
         errPromiseCallback().catch((error) => {
-            console.log(error.message);
+            console.log("catch1 " + error.message);
+            //throw error;
         });
     } catch (error) {
-        console.log(error.message);
+        console.log("catch2 " + error.message);
     }
 
 
     try {
         errAsyncFunction().catch((error) => {
-            console.log(error.message);
+            console.log("catch1 " + error.message);
+            //throw error
         });
     } catch (error) {
-        console.log(error.message);
+        console.log("catch2 " + error.message);
     }
 
     try {
         errAsyncFunctionCallback().catch((error) => {
-            console.log(error.message);
+            console.log("catch1 " + error.message);
+            //throw error
         });
     } catch (error) {
-        console.log(error.message);
+        console.log("catch2 " + error.message);
     }
 
 }
