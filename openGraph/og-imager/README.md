f47a67f9b122024e8be249a555ead92c0632a216
Oriol Amat <oriol.amat@gmail.com>
2022-01-28 22:58:57 +0100

    openGraphImg

---
+++ openGraph/og-imager/README.md
@@ -0,0 +1,4 @@
+# Open Graph Imager example Generator with NodeJS
+
+## node server.js
+## Call http://localhost:3000/ogimage?title=Open%20Graph%20Imager%20-%20NodeJS%20Generator&tags[]=og-imager&tags[]=with_nodejs&path=https://github.com/oamat/nodejs/openGraph/og-imager
---
+++ openGraph/og-imager/server.js
@@ -0,0 +1,39 @@
+
+// server.js
+const express = require("express");
+const generateImage = require("./utils/generateImage");
+const { getCompiledHTML } = require("./utils/compileTemplate");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+app.get('/', (req, res) => {
+  const compiledHTML = getCompiledHTML(req.query);
+
+  res.status(200).send(compiledHTML);
+})
+
+
+app.get('/ogimage', async (req, res) => {
+  try {
+    const compiledHTML = getCompiledHTML(req.query);
+  
+    const image = await generateImage({
+      width: req.query.width,
+      height: req.query.height,
+      content: compiledHTML
+    });
+      
+    res.writeHead(200, { 'Content-Type': 'image/png', 'Cache-Control': `public, immutable, no-transform, s-max-age=2592000, max-age=2592000` });
+    res.end(image);
+  } catch(e) {
+    console.log(e);
+    res.status(500).send('Internal Server Error!')
+  }
+});
+
+
+app.listen(port, () => {
+  console.log(`og-imager listening at ${port}`)
+});
