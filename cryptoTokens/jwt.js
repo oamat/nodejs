edeedf42b77918ce9f3a0c47248118400e0b34c2
Oriol Amat <oriol.amat@gmail.com>
2022-10-15 07:35:40 +0200

    jwt_public_private

---
+++ cryptoTokens/jwt.js
@@ -0,0 +1,41 @@
+'use strict';
+const fs = require('fs');
+const jwt = require('jsonwebtoken');
+var privateKEY = fs.readFileSync('./private.key', 'utf8');
+var publicKEY = fs.readFileSync('./public.key', 'utf8');
+/*
+ ====================   JWT Signing =====================
+*/
+
+console.log(privateKEY);
+var payload = {
+    data1: "Data 1",
+    data2: "Data 2",
+    data3: "Data 3",
+    data4: "Data 4",
+};
+var i = 'CaixaBank';
+var s = 'caixa@caixabank.com';
+var a = 'http://www.caixabank.es';
+
+var signOptions = {
+    issuer: i,
+    subject: s,
+    audience: a,
+    expiresIn: "12h",
+    algorithm: "RS256"   // RSASSA [ "RS256", "RS384", "RS512" ]
+};
+var token = jwt.sign(payload, privateKEY, signOptions);
+console.log("Token :" + token);
+/*
+ ====================   JWT Verify =====================
+*/
+var verifyOptions = {
+    issuer: i,
+    subject: s,
+    audience: a,
+    expiresIn: "12h",
+    algorithm: ["RS256"]
+};
+var legit = jwt.verify(token, publicKEY, verifyOptions);
+console.log("\nJWT verification result: " + JSON.stringify(legit));
